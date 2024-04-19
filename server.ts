f372ef73fbf1e72e4a982eda736eae1100456c8f
DESKTOP-NPUVJIQ\Mark 1 <tabish169000@gmail.com>
2024-04-19 03:12:46 -0700

    added prettier setup and their respective file

--- server.ts
+++ server.ts
@@ -1,9 +1,21 @@
-const http=require('http')
+const http = require('http');
 
-http.createServer((req: any, res:any) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World\n')
-}).listen(1337, '127.0.0.1')
-console.log('Server running at http://127.0.0.1:1337/')
+http
+  .createServer((req: any, res: any) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+  })
+  .listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
 
+console.log('This is data ');
 
+function add() {
+  console.log('yrd');
+
+  console.log('yes');
+
+  console.log('Nothing');
+
+  console.log('nnnnnnnnnnnnnnnnnn');
+}
